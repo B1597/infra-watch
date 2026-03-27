bfa5211c506466d82813fc2e8090194d6181da0c
Bojana Dimovikj <bojana.dimovik@live.com>
2026-03-27 16:36:36 +0100

    add collapsible sidebar and node detail layout scaffold

--- src/app/features/topology/components/node-detail/node-detail.component.ts
+++ src/app/features/topology/components/node-detail/node-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-node-detail',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './node-detail.component.html',
   styleUrl: './node-detail.component.scss',
 })
--- src/app/features/topology/pages/topology-page/topology-page.component.ts
+++ src/app/features/topology/pages/topology-page/topology-page.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TopologyTreeComponent } from '../../components/topology-tree/topology-tree.component';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-topology-page',
-  imports: [],
+  imports: [RouterOutlet, TopologyTreeComponent, MatIcon],
   templateUrl: './topology-page.component.html',
   styleUrl: './topology-page.component.scss',
 })
-export class TopologyPageComponent {}
+export class TopologyPageComponent {
+  collapsed = signal(false);
+  toggle() { this.collapsed.update(v => !v); }
+}
